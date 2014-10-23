abb3985ddcb3b8da61dda667681668e2dd1bf6b4
Nicholas Manos <manos.nick@gmail.com>
2014-10-23 10:08:58 -0700

    test and queue fix

--- lib/queue.rb
+++ lib/queue.rb
@@ -9,6 +9,7 @@ class Queue
   end
 
   def dequeue
+   return 'The queue is empty, dequeue failed.' if $list.size == 0
    return $list.remove($list.tail.data).data
   end
 
@@ -18,18 +19,21 @@ class Queue
 
 end
 
-# queue = Queue.new
-# queue.enqueue(5)
-# queue.enqueue(4)
-# queue.enqueue(3)
-# queue.enqueue(2)
-# queue.enqueue(1)
-# puts queue.size
-# puts queue.dequeue
-# puts queue.size
-# puts queue.dequeue
-# puts queue.size
-# puts queue.dequeue
-# puts queue.size
-# puts queue.dequeue
-# puts queue.size
+queue = Queue.new
+queue.enqueue(5)
+queue.enqueue(4)
+queue.enqueue(3)
+queue.enqueue(2)
+queue.enqueue(1)
+puts queue.size
+puts queue.dequeue
+puts queue.size
+puts queue.dequeue
+puts queue.size
+puts queue.dequeue
+puts queue.size
+puts queue.dequeue
+puts queue.size
+puts queue.dequeue
+puts queue.dequeue
+puts queue.dequeue
